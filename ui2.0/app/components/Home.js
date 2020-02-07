a21067b08fb3042b1724e29bcc0f40f52b453784
Bryan ZHU <bryanzhu@BRYAN-MBP.local>
2020-02-07 03:55:04 -0500

    fix typed react loading bug

--- ui2.0/app/components/Home.js
+++ ui2.0/app/components/Home.js
@@ -12,7 +12,7 @@ export default class Home extends Component<Props> {
   render() {
     return (
       <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
+        <img src="./resources/sample.gif" alt="sample.gif" />
         <Link to={routes.COUNTER}>to Counter</Link>
       </div>
     );
