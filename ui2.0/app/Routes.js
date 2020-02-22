1d39ffb14fbb248aec544f4bbb85f2ed9aafd569
Bryan ZHU <bryanzhu@BRYAN-MBP.local>
2020-02-22 18:46:39 -0500

    create home page

--- ui2.0/app/Routes.js
+++ ui2.0/app/Routes.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import routes from './constants/routes.json';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
+import Home from './containers/Home';
 import CounterPage from './containers/CounterPage';
 
 export default () => (
   <App>
     <Switch>
       <Route path={routes.COUNTER} component={CounterPage} />
-      <Route path={routes.HOME} component={HomePage} />
+      <Route path={routes.HOME} component={Home} />
     </Switch>
   </App>
 );
---
+++ ui2.0/app/components/Homepage/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import homepageIMG from 'images/homepage.svg';
+
+export default () => (
+  <>
+    <img src={homepageIMG} alt="homepage" />
+  </>
+);
